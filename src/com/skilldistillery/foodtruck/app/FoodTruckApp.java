271eb3b58245717a87097195187292f2afb6156f
Katherine <remick.ka@gmail.com>
2022-03-25 19:39:34 -0700

    Working Code

--- src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -1,10 +1,101 @@
 package com.skilldistillery.foodtruck.app;
+import com.skilldistillery.foodtruck.entities.*;
+import java.util.Scanner;
 
 public class FoodTruckApp {
+	public FoodTruckApp() {}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Welcome to RATE THE TRUCK, where you can rate 5 food trucks of your choice!\n");
+		FoodTruckApp fta = new FoodTruckApp();
+		FoodTruck[] fleet = fta.userEntries();
+		if( fleet[0] == null) {
+			System.out.println("No food trucks have been rated. Have a nice day!");
+		}else {
+			fta.reviewMenu(fta, fleet);
+			
+		}
+		
+		
 	}
-
+	
+	
+	public FoodTruck[] userEntries() {
+		Scanner kb = new Scanner(System.in);
+		FoodTruck[] fleet = new FoodTruck[5];
+		String nameQ = "What is the name of the food truck you are rating?\n";
+		String name = "";
+		String foodQ = "What is the food that they serve?";
+		String food = "";
+		String ratingQ= "What (out of ten) do you rate the food truck?";
+		double rating = 0.0;
+		String[] prompts = {nameQ, foodQ, ratingQ};
+		String userInput = ""; 
+		
+		
+		for( int i = 0; i < 5; i++) {
+			System.out.println("Let's rate a truck! Type in 'quit' to stop rating.");
+			for( int q = 0; q < prompts.length; q ++) {
+				System.out.println(prompts[q]);
+				userInput = kb.nextLine();
+				if (userInput.equalsIgnoreCase("quit")) {
+					i = 4;
+					break;
+				}
+				switch(q){
+				case 0: 
+					name = userInput;
+					break;
+				case 1:
+					food = userInput;
+					break;
+				case 2:
+					rating = Double.parseDouble(userInput);
+					break;
+				}
+			}
+			fleet[i] = new FoodTruck(name, food, rating);
+		}
+		
+		return fleet;
+		
+	}
+	private void displayMenu() {
+		System.out.println("Would you like to...");
+		System.out.println("1) Look at your list of existing food trucks.");
+		System.out.println("2) See the average rating of the food trucks.");
+		System.out.println("3) View the highest rated food truck.");
+		System.out.println("4) Exit");
+	}
+	
+	public void reviewMenu(FoodTruckApp fta, FoodTruck[]  fleet) {
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Wow! those were some pretty trucklike Food Trucks!");
+		int userInput = 0;
+		do{
+			fta.displayMenu();
+			userInput = kb.nextInt();
+			switch(userInput) {
+			case 1:
+				fleet[0].toString(fleet);
+				break;
+			case 2:
+				System.out.println("The fleet average is: "+ fleet[0].calculateAverage(fleet) ) ;
+				break;
+			case 3:
+				System.out.println("The food truck with the hightest rating is..." + fleet[0].highestRating(fleet) + "!");
+				break;
+			case 4:
+				System.out.println("Thank you for your input! Goodbye.");
+				break;
+			default:
+				System.out.println("That input is not recognized, please enter a number (1-4)");
+				
+			}
+		}while(userInput !=4);
+		
+		
+	}
 }
--- src/com/skilldistillery/foodtruck/entities/FoodTruck.java
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -1,5 +1,75 @@
 package com.skilldistillery.foodtruck.entities;
 
 public class FoodTruck {
+	private String name;
+	private String food;
+	private double rating;
+	private int id;
+	
 
+
+	public static int truckNum = 0;
+	
+	public FoodTruck() {}
+	public FoodTruck(String name, String food, double rating) {
+		this.name = name;
+		this.food = food;
+		this.rating = rating;
+		if (rating > 10) {
+			rating %= 10;
+		}
+		this.id = truckNum + 1;
+		truckNum++;
+		
+		
+		
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public double getRating() {
+		return rating;
+	}
+	public String getFood() {
+		return food;
+	}
+	public int getId() {
+		return id;
+	}
+	
+	public double calculateAverage(FoodTruck [] fleet) {
+		double total = 0;
+		for(int i = 0; i < fleet.length; i++ ) {
+			total += fleet[i].rating;
+		}
+		double average = (Math.round(total/truckNum)*10.0)/10.0;
+		return average;
+	}
+	
+	
+	public String highestRating(FoodTruck[] fleet) {
+		double highest = 0;
+		int highestI = 0;
+		for( int i  = 0; i < fleet.length; i ++) {
+			if ( fleet[i].getRating() > highest) {
+				highest = fleet[i].getRating();
+				highestI = i;
+			}
+		}
+		return fleet[highestI].getName();
+	}
+	
+	
+	public void toString(FoodTruck[] fleet) {
+		
+		for(int i = 0; i < fleet.length; i ++) {
+			if (fleet[i] == null) {
+				break;
+			}
+			System.out.println(fleet[i].getId() +  ") " + fleet[i].getName() + " is selling " + fleet[i].getFood() + " and is rated: " + fleet[i].getRating()+ ".");
+		}
+		
+	}
+	
 }
